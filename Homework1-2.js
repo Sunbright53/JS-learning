c3bd6987f6b124a135bf0c90de849ca8bf9d0ff0
Sunbright53 <141810435+Sunbright53@users.noreply.github.com>
2024-05-17 17:43:22 +0700

    feat:JS read&write

---
+++ Homework1-2.js
@@ -0,0 +1,14 @@
+let celsius = 30;
+celsius = 50;
+let fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+const dog = 10;
+const cat = 20;
+console.log(dog + cat);
+
+let fish = 5;
+let cow = 4;
+let sell = fish - 2 + (cow - 3);
+let total = sell;
+console.log(total);
---
+++ variable-value/value.js
@@ -0,0 +1,13 @@
+const TwoLegs = "bird";
+console.log(TwoLegs); //Expression =>
+const Fourlegs = "dog";
+console.log(Fourlegs); //Expression=>
+console.log(10 + 15); //Expression=>
+
+let a = 3 > 2;
+console.log(3 > 2); //Expression=>
+console.log(a); //Expression=>
+
+const b = 1 * 1 + 8 > 2 * (50 - 4);
+console.log(1 * 1 + 8 > 2 * (50 - 4)); //Expression=>
+console.log(b); //Expression =>
